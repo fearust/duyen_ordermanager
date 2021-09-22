e9258045a74a16a0a84b387583e0a7d83c5369cc
fearust <fearust@gmail.com>
2021-09-22 17:46:14 +0900

    change setting

--- duyen/settings/prod.py
+++ duyen/settings/prod.py
@@ -2,3 +2,17 @@ from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['3.37.207.165']
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'duyen',
+        'USER': 'dbmasteruser',
+        'PASSWORD': '+N*=|H5|_`f6R{-TVMLBwOd77J]gLp-,',
+        'HOST': 'ls-9e9555d52f41681ed51b6d56b329ad40eaedac00.cbuobpiqxmfb.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
