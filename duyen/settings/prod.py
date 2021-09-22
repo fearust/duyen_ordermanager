cd72d211a3582650e595d9dbf86cf4efb8eb597a
fearust <fearust@gmail.com>
2021-09-22 17:52:19 +0900

    change setting

--- duyen/settings/prod.py
+++ duyen/settings/prod.py
@@ -13,6 +13,6 @@ DATABASES = {
         'USER': 'dbmasteruser',
         'PASSWORD': '+N*=|H5|_`f6R{-TVMLBwOd77J]gLp-,',
         'HOST': 'ls-9e9555d52f41681ed51b6d56b329ad40eaedac00.cbuobpiqxmfb.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '5432',
+        'PORT': '5432'
     }
 }
