2b9e808353e61d26dd38550782079b259afabd75
fearust <fearust@gmail.com>
2021-09-22 18:58:28 +0900

    change setting

--- duyen/settings/base.py
+++ duyen/settings/base.py
@@ -69,10 +69,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'duyen.wsgi.application'
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'duyen',
+        'USER': 'dbmasteruser',
+        'PASSWORD': '+N*=|H5|_`f6R{-TVMLBwOd77J]gLp-,',
+        'HOST': 'ls-9e9555d52f41681ed51b6d56b329ad40eaedac00.cbuobpiqxmfb.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -113,8 +121,6 @@ LOGOUT_REDIRECT_URL = '/common/login/'
 SIMPLEMDE_OPTIONS = {
     'placeholder': '내용을 입력하세요',
     'status': False,
-    'autosave': {
-        'enabled': True
-    },
+    'autosave': {'enabled': True},
     'spellChecker': False,
 }
